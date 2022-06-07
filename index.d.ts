c4e5bdcbf5d3d117d1fcf4035edb45cca06f2174
Han Wu <xjohnwu@gmail.com>
2022-06-07 14:54:43 +0800

    cryptopian update
    1. websocket objects field consistency:
    orderStatus -> status, averagePrice -> avgPrice
    2. Make recvWindow optional
    3. futuresCancelOrder accept origClientOrderId
    4. update module name to @cryptopian001/binance-api-node
    5. add publishConfig

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 // tslint:disable:interface-name
-declare module 'binance-api-node' {
+declare module '@cryptopian001/binance-api-node' {
   export default function(options?: {
     apiKey?: string
     apiSecret?: string
@@ -564,7 +564,7 @@ declare module 'binance-api-node' {
       startTime?: number
       endTime?: number
       limit?: number
-      recvWindow: number
+      recvWindow?: number
     }): Promise<QueryOrderResult[]>
     dailyStats(options?: { symbol: string }): Promise<DailyStatsResult | DailyStatsResult[]>
     candles(options: CandlesOptions): Promise<CandleChartResult[]>
@@ -674,11 +674,21 @@ declare module 'binance-api-node' {
     getMultiAssetsMargin(): Promise<MultiAssetsMargin>
     setMultiAssetsMargin(options: MultiAssetsMargin): Promise<MultiAssetsMargin>
 
-    futuresCancelOrder(options: {
-      symbol: string
-      orderId: number
-      useServerTime?: boolean
-    }): Promise<CancelOrderResult>
+    futuresCancelOrder(
+      options:
+        | {
+            symbol: string
+            orderId: number
+            recvWindow?: number
+            useServerTime?: boolean
+          }
+        | {
+            symbol: string
+            origClientOrderId: string
+            recvWindow?: number
+            useServerTime?: boolean
+          },
+    ): Promise<CancelOrderResult>
     futuresCancelAllOpenOrders(options: {
       symbol: string
     }): Promise<FuturesCancelAllOpenOrdersResult>
@@ -698,6 +708,7 @@ declare module 'binance-api-node' {
     }): Promise<QueryFuturesOrderResult>
     futuresOpenOrders(options: {
       symbol?: string
+      recvWindow?: number
       useServerTime?: boolean
     }): Promise<QueryFuturesOrderResult[]>
     futuresAllOrders(options: {
@@ -714,14 +725,14 @@ declare module 'binance-api-node' {
     }): Promise<PositionRiskResult[]>
     futuresLeverageBracket(options?: {
       symbol?: string
-      recvWindow: number
+      recvWindow?: number
     }): Promise<LeverageBracketResult[]>
     futuresAccountBalance(options?: { recvWindow: number }): Promise<FuturesBalanceResult[]>
     futuresAccountInfo(options?: { recvWindow: number }): Promise<FuturesAccountInfoResult>
     futuresPositionMode(options?: { recvWindow: number }): Promise<PositionModeResult>
     futuresPositionModeChange(options: {
       dualSidePosition: string
-      recvWindow: number
+      recvWindow?: number
     }): Promise<ChangePositionModeResult>
     futuresLeverage(options: {
       symbol: string
@@ -1428,12 +1439,13 @@ declare module 'binance-api-node' {
     FULL = 'FULL',
   }
 
-  export type TimeInForce_LT = 'GTC' | 'IOC' | 'FOK' | 'GTE_GTC'
+  export type TimeInForce_LT = 'GTC' | 'IOC' | 'FOK' | 'GTX' | 'GTE_GTC'
 
   export const enum TimeInForce {
     GTC = 'GTC',
     IOC = 'IOC',
     FOK = 'FOK',
+    GTX = 'GTX',
     GTE_GTC = 'GTE_GTC'
   }
 
@@ -1675,7 +1687,7 @@ declare module 'binance-api-node' {
     orderId: number // Order ID
     orderListId: number // OrderListId
     orderRejectReason: OrderRejectReason // Order reject reason; will be an error code.
-    orderStatus: OrderStatus_LT // Current order status
+    status: OrderStatus_LT // Current order status
     orderTime: number // Transaction time
     orderType: FuturesOrderType_LT // Order type
     originalClientOrderId: string | null // Original client order ID; This is the ID of the order being canceled
@@ -1767,10 +1779,10 @@ declare module 'binance-api-node' {
     timeInForce: TimeInForce
     quantity: string
     price: string
-    averagePrice: string
+    avgPrice: string
     stopPrice: string
     executionType: ExecutionType
-    orderStatus: OrderStatus
+    status: OrderStatus
     orderId: number
     lastTradeQuantity: string
     totalTradeQuantity: string
@@ -2004,7 +2016,7 @@ declare module 'binance-api-node' {
     price: string
     origQty: string
     executedQty: string
-    averagePrice: string
+    avgPrice: string
     status: string
     timeInForce: string
     type: OrderType_LT
@@ -2307,7 +2319,7 @@ declare module 'binance-api-node' {
     origQty: string
     lastFilledQty: string
     accumulatedQty: string
-    averagePrice: string
+    avgPrice: string
     status: string
     timeInForce: string
     type: OrderType_LT
